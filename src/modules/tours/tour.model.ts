d8b697a78cf0eadde227fe1ceded0501bb4844af
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-03 18:23:39 +0300

    add additional tour schema fields

--- src/modules/tours/tour.model.ts
+++ src/modules/tours/tour.model.ts
@@ -5,6 +5,49 @@ const tourSchema = new Schema({
     type: String,
     required: [true, 'Tour name is required'],
     unique: true,
+    trim: true,
+  },
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a description'],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  price: Number,
+
+  priceDiscount: Number,
+
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have a difficulty'],
+  },
+
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
 
   ratings: {
@@ -12,7 +55,20 @@ const tourSchema = new Schema({
     required: [true, 'You must provide a rating'],
   },
 
-  price: Number,
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have a cover image'],
+  },
+
+  images: [String],
+
+  startDates: [Date],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
 });
 
 export type Tour = InferSchemaType<typeof tourSchema>;
