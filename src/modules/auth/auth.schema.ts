6b071e710618432eb5c173ac1e1a3efa254d5606
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-19 15:14:03 +0300

    add login schema to the list of auth schemas

--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -2,14 +2,32 @@ import { z } from 'zod';
 
 import { zodIssue } from '../../utils/zod-utils';
 
+export const emailSchema = z
+  .string({
+    error: zodIssue('Email is required', 'Email should be string'),
+  })
+  .min(1, { error: 'Email is required' })
+  .pipe(z.email({ error: 'Please provide a valid email address' }));
+
+export const passwordSchema = z
+  .string({
+    error: zodIssue('Password is required', 'Password must be string'),
+  })
+  .min(1, { error: 'Password is required' })
+  .min(8, { error: 'Password must be at least 8 characters long' })
+  .regex(/[A-Z]/, {
+    error: 'Password must contain at least one uppercase letter',
+  })
+  .regex(/[a-z]/, {
+    error: 'Password must contain at least one lowercase letter',
+  })
+  .regex(/\d/, {
+    error: 'Password must contain at least one number',
+  });
+
 export const signupSchema = z
   .object({
-    email: z
-      .string({
-        error: zodIssue('Email is required', 'Email should be string'),
-      })
-      .min(1, { error: 'Email is required' })
-      .pipe(z.email({ error: 'Please provide a valid email address' })),
+    email: emailSchema,
 
     name: z
       .string({
@@ -19,22 +37,7 @@ export const signupSchema = z
 
     avatar: z.url({ error: 'Avatar url should be string' }).optional(),
 
-    password: z
-      .string({
-        error: zodIssue('Password is required', 'Password must be string'),
-      })
-      .min(1, { error: 'Password is required' })
-      .min(8, { error: 'Password must be at least 8 characters long' })
-      .regex(/[A-Z]/, {
-        error: 'Password must contain at least one uppercase letter',
-      })
-      .regex(/[a-z]/, {
-        error: 'Password must contain at least one lowercase letter',
-      })
-      .regex(/\d/, {
-        error: 'Password must contain at least one number',
-      }),
-
+    password: passwordSchema,
     confirmPassword: z
       .string({ error: 'Please confirm your password' })
       .min(1, 'Please confirm your password'),
@@ -44,4 +47,10 @@ export const signupSchema = z
     message: "Passwords don't match",
   });
 
+export const loginSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+});
+
 export type SignupSchema = z.infer<typeof signupSchema>;
+export type LoginSchema = z.infer<typeof loginSchema>;
